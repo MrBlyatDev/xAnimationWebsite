f8bae8ad337ca4f253fa85b13a6990e9d41684a1
Yerasyl Muratkaliuly <dainsnba@gmail.com>
2025-07-12 08:37:36 +0500

    Implement Hero Scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
 import { useGSAP } from "@gsap/react"
 import { SplitText } from "gsap/all"
 import gsap from "gsap";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+  const isMobile = useMediaQuery({maxWidth: 777});
   
   useGSAP(() => {
     const heroSplit = new SplitText('.title', { type: 'chars, words' });
@@ -35,11 +39,33 @@ const Hero = () => {
       }
     })
     .to('.left-leaf', { y: 300 }, 0)
-    .to('.right-leaf', { y: -300 }, 0) 
+    .to('.right-leaf', { y: -300 }, 0)
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120% top' : 'bottom top';
+
+    const timelineRef = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      }
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      timelineRef.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+        ease: 'none',
+        duration: videoRef.current.duration,
+      });
+    }
   });
   
   return (
     <>
+      
       <section id="hero" className='noisy'>
 
         <h1 className="title">MOJITO</h1>
@@ -65,6 +91,17 @@ const Hero = () => {
         </div>
       
       </section>
+
+      <section className="video absolute inset-0">
+        <video 
+        ref={videoRef} 
+        src="../public/videos/output.mp4"
+        muted
+        playsInline
+        preload="auto"
+        />
+      </section>
+
     </>
   )
 }
